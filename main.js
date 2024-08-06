9136fe9a064dee48a8564687c212c30bad2f723f
ldevroye <54361927+ldevroye@users.noreply.github.com>
2024-08-06 20:11:57 +0200

    [FIX] import path

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
-import * as THREE from 'three';
-import { GLTFLoader } from 'three/addons/loaders/GLTFLoader.js';
+import * as THREE from 'https://cdn.jsdelivr.net/npm/three@0.150.0/build/three.module.js';
+import { GLTFLoader } from 'https://cdn.jsdelivr.net/npm/three@0.150.0/examples/jsm/loaders/GLTFLoader.js';
 
 let scene, camera, renderer, cube;
 
